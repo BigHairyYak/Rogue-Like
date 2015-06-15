15c351b3eac3316f2636fc7369863c32e15491ec
kguo0311 <kguo0311@yahoo.com>
2015-06-15 12:24:33 -0400

    Kevin's stuff
    first commit

---
+++ src/davidchen/Main.java
@@ -0,0 +1,39 @@
+package davidchen;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+
+public class Main extends JApplet{
+	RogueLikeStart startPlay;
+	RogueLikeStart stop;
+	Image image;
+	Toolkit tk;
+	String gameString = "Izudu";
+	public void init(){
+		Container c = getContentPane();
+		tk = Toolkit.getDefaultToolkit();
+		setSize(1500,1400);
+		setContentPane(new DrawingPanel());
+		setVisible(true);
+
+		image = tk.getImage("Rogue legacy logo.jpg");
+		startPlay = new RogueLikeStart(560, 370, 180, 80, "Start", Color.green, this);
+		stop = new RogueLikeStart(560, 520, 180, 80, "Exit", Color.red, this);
+		setLayout(null);
+	}
+
+	//Creates an inner class that extends a JPanel
+	public class DrawingPanel extends JPanel
+	{
+		public void paintComponent(Graphics g){
+			startPlay.draw(g);
+			stop.draw(g);
+			g.setFont(g.getFont().deriveFont(24.0f));
+			g.drawString(gameString, 620, 770);
+			g.drawImage(image, 520, 50, null);
+			repaint();
+		}
+	}
+}
---
+++ src/davidchen/RogueLikeStart.java
@@ -0,0 +1,61 @@
+package davidchen;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.Rectangle;
+
+public class RogueLikeStart implements MouseListener
+{
+	Rectangle r, rStart, rStop;
+	int x, y, height, width, mouseX, mouseY;
+	String selection;
+	Color backColor;
+	Boolean buttonClicked = false;
+
+	//Creates a button object
+	public RogueLikeStart(int x, int y, int width, int height, String selection, Color backColor, JApplet app)
+	{
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.width = width;
+		this.selection = selection;
+		this.backColor = backColor;
+		r = new Rectangle(x, y, width, height);
+
+		app.addMouseListener(this);
+	}
+
+	//Draws a button based on the coordinates entered
+	public void draw(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(backColor);
+		g2.draw(r);
+		g2.fill(r);
+
+		g2.setColor(Color.black);
+		g2.setFont(g2.getFont().deriveFont(18.0f));
+		g2.drawString(selection, x+(width/3)+13, y+(height/2)+5);
+	}
+
+	//Detects collision by mouse
+	public void mouseClicked(MouseEvent e){
+		mouseX = e.getX();
+		mouseY = e.getY();
+
+		//If the click is inside the rectangle, click is returned as true
+		if(r.contains(mouseX,  mouseY))
+		{
+			buttonClicked = true;
+			System.out.println(selection+" button is clicked.");
+		}
+
+	}
+
+	public void mousePressed(MouseEvent e){}
+	public void mouseReleased(MouseEvent e){}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+}
